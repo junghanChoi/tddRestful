fb0b692afe28df923edac7d6e2d21c368479f8e0
David <david@Davidui-MacBookPro.local>
2019-06-30 12:56:46 +0900

    ch6 done

---
+++ modules/customer/customer.module.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+    module.exports = init;
+
+    function init(){
+        return {
+             CustomerController : require('./customer.controller'),
+             CustomerMiddleware : require('./customer.middleware'),
+             CustomerService : require('./customer.service'),
+             CustomerModel: require('./customer.model')
+        };
+    }
+})();
